6362c7f3dab9d4d02cfe3f9983aa0ce72d28feac
Dylan Dey <ddey117@utexas.edu>
2022-02-22 18:25:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name = 'preprocess_ddey117',  #must be unique
     version = '0.0.1',
     author = 'Dylan Dey',
-    author_email = 'ddey2985@gmail.com'
+    author_email = 'ddey2985@gmail.com',
     description = 'Text data preprocessing package',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
